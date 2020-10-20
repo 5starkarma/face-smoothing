50b0cccf2757d1da800f567aae76c24006f2870e
5starkarma <davidalford678@gmail.com>
2020-10-20 17:09:44 +0000

    Clean up

--- utils/image.py
+++ utils/image.py
@@ -1,3 +1,9 @@
+# Save image adapted from https://stackoverflow.com/a/56680778/10796680
+
+# System imports
+import os
+
+#Third-party imports
 import cv2
 
 
@@ -16,3 +22,25 @@ def load_image(path):
         RGB image
     """
     return cv2.imread(path)
+
+
+def save_image(output_dir, filename, img):
+    """
+    Save an image using OpenCV
+
+    Parameters
+    ----------
+    filename : str
+        Name to save image as
+
+    Returns
+    -------
+    Bool : bool
+        True if image save was success
+    """
+    counter = 0
+    filename = os.path.join(output_dir, filename + '{}.jpg')
+    while os.path.isfile(filename.format(counter)):
+        counter += 1
+    filename = filename.format(counter)
+    return cv2.imwrite(filename, img)
