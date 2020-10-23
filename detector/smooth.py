35ceb2cec7011d01178432e5c64f8d8a1511674b
5starkarma <davidalford678@gmail.com>
2020-10-23 17:35:58 +0000

    Change function names

--- detector/smooth.py
+++ detector/smooth.py
@@ -44,11 +44,11 @@ def smooth_face(cfg, detected_img, bboxes):
     for box_num in range(len(bboxes)):
         print(f'Face detected: {bboxes[box_num]}')
         # Get Region of Interest
-        roi = get_roi(detected_img, bboxes, box_num)
+        roi_img = get_roi(detected_img, bboxes, box_num)
         # Copy ROI
-        temp_img = roi.copy()
+        temp_img = roi_img.copy()
         # Convert roi_img to HSV colorspace
-        hsv_img = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        hsv_img = cv2.cvtColor(roi_img, cv2.COLOR_BGR2HSV)
         # Get the mask for calculating histogram of the object and remove noise
         hsv_mask = cv2.inRange(hsv_img, 
                                np.array(cfg['image']['hsv_low']), 
@@ -56,7 +56,7 @@ def smooth_face(cfg, detected_img, bboxes):
         # Make a 3 channel mask
         full_mask = cv2.merge((hsv_mask, hsv_mask, hsv_mask))
         # Apply blur on the created image
-        blurred_img = cv2.bilateralFilter(roi, 
+        blurred_img = cv2.bilateralFilter(roi_img, 
                                           cfg['filter']['diameter'], 
                                           cfg['filter']['sigma_1'], 
                                           cfg['filter']['sigma_2'])
@@ -67,10 +67,10 @@ def smooth_face(cfg, detected_img, bboxes):
         # Created anti-mask
         masked_img2 = cv2.bitwise_and(temp_img, inverted_mask)
         # Add the masked images together
-        full_img = cv2.add(masked_img2, masked_img)
+        smoothed_roi = cv2.add(masked_img2, masked_img)
         # Init smoothed image
-        smoothed_img = detected_img.copy()
+        output_img = detected_img.copy()
         # Replace ROI on full image with blurred ROI
-        smoothed_img[bboxes[box_num][1]:bboxes[box_num][3], 
-                     bboxes[box_num][0]:bboxes[box_num][2]] = full_img
-    return smoothed_img, roi, full_mask, full_img
+        output_img[bboxes[box_num][1]:bboxes[box_num][3], 
+                   bboxes[box_num][0]:bboxes[box_num][2]] = smoothed_roi
+    return output_img, roi_img, full_mask, smoothed_roi
